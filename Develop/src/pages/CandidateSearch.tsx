13f84708b35dcdf269c5d5d2e6dceb171fdeebd2
Kayla Hebertson <kayla.hebertson@gmail.com>
2024-12-09 16:42:29 -0700

    save candidate funciton

--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
-import type { Candidate } from '../types/Candidate';
+import type { Candidate } from '../interfaces/Candidate.interface';
+import CandidateCard from '../components/CandidateCard';
 
 const CandidateSearch = () => {
   const [results, setResults] = useState<Candidate[]>([]);
@@ -26,10 +27,38 @@ const CandidateSearch = () => {
     const users: Candidate[] = await searchGithub();
     setResults(users);
     await searchSpecificUser(users[currentIndex].login || '');
-  }
+  };
 
-const CandidateSearch = () => {
-  return <h1>CandidateSearch</h1>;
+  const addCandidate = async (isSelected: boolean) => {
+    if (isSelected) {
+      let parsedCandidates = Candidate[] = [];
+      const SavedCandidates = localStorage.getItem('SavedCandidates');
+      if (typeof SavedCandidates === 'string') {
+        parsedCandidates = JSON.parse(SavedCandidates);
+      }
+      parsedCandidates.push(currentUser);
+      localStorage.setItem('SavedCandidates', JSON.stringify(parsedCandidates));
+    }
+    if (currentIndex < results.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+      await searchSpecificUser(results[currentIndex + 1].login || '');
+    } else {
+      setCurrentIndex(0);
+      await searchForUsers();
+    }
+  };
+
+  useEffect(() => {
+    searchForUsers();
+    searchSpecificUser(currentUser.login || '');
+  }, []);
+
+  return (
+    <>
+      </><h1>CandidateSearch</h1>
+      <CandidateCard currentUser={currentUser} addCandidate={addCandidate} />
+    </>
+  );
 };
 
 export default CandidateSearch;
