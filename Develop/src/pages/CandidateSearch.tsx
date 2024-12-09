cac07171834bb7601cfe8c200363c9c8e690fe35
Kayla Hebertson <kayla.hebertson@gmail.com>
2024-12-09 16:28:17 -0700

    search user functions

--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -1,5 +1,32 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import type { Candidate } from '../types/Candidate';
+
+const CandidateSearch = () => {
+  const [results, setResults] = useState<Candidate[]>([]);
+  const [currentUser, setCurrentUser] = useState<Candidate>({
+    id: null,
+    login: null,
+    email: null,
+    html_url: null,
+    name: null,
+    bio: null,
+    company: null,
+    location: null,
+    avatar_url: null,
+  });
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+
+  const searchSpecificUser = async (username: string) => {
+    const user: Candidate = await searchGithubUser(username);
+    setCurrentUser(user);
+  };
+
+  const searchForUsers = async () => {
+    const users: Candidate[] = await searchGithub();
+    setResults(users);
+    await searchSpecificUser(users[currentIndex].login || '');
+  }
 
 const CandidateSearch = () => {
   return <h1>CandidateSearch</h1>;
